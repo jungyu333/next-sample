d6ac50ad5668fea4cf208c27abcbc9dac04bf073
jungyu333 <jungyu3826@naver.com>
2023-06-17 13:50:13 +0900

    main test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <div>Home</div>
-      <div>testrdddrrddd</div>
+      <div>main</div>
       <div>{process.env.NEXT_PUBLIC_API_URL}</div>
     </>
   );
